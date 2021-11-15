9d7bee81ee7c6172e8d178a00597625749552afb
Giancarlo Mennillo <giancarlo.mennillo@safectory.com>
2021-11-15 23:26:00 +0100

    align code add stopwatch

--- lib/canvas_area/canvas_area.dart
+++ lib/canvas_area/canvas_area.dart
@@ -14,22 +14,32 @@ class CanvasArea extends StatefulWidget {
 class _CanvasAreaState<CanvasArea> extends State {
   int score = 0;
   List<Beer> beers = List();
+  Stopwatch _stopwatch;
+
+  String formatTime(int milliseconds) {
+    var secs = milliseconds ~/ 1000;
+    var hours = (secs ~/ 3600).toString().padLeft(2, '0');
+    var minutes = ((secs % 3600) ~/ 60).toString().padLeft(2, '0');
+    var seconds = (secs % 60).toString().padLeft(2, '0');
+    return "$hours:$minutes:$seconds";
+  }
 
   @override
   void initState() {
     _spawnRandomBeers();
     _tick();
+    _stopwatch = Stopwatch();
     super.initState();
   }
 
   void _spawnRandomBeers() {
     beers.add(new Beer(
-      position: Offset(0, 200),
-      width: 80,
-      height: 80,
-      additionalForce: Offset(5 + Random().nextDouble() * 5, Random().nextDouble() * -10),
-      rotation: Random().nextDouble() / 3 - 0.16
-    ));
+        position: Offset(0, 200),
+        width: 80,
+        height: 80,
+        additionalForce:
+            Offset(5 + Random().nextDouble() * 5, Random().nextDouble() * -10),
+        rotation: Random().nextDouble() / 3 - 0.16));
   }
 
   void _tick() {
@@ -37,7 +47,6 @@ class _CanvasAreaState<CanvasArea> extends State {
       for (Beer beer in beers) {
         beer.applyGravity();
       }
-
 
       if (Random().nextDouble() > 0.97) {
         _spawnRandomBeers();
@@ -49,91 +58,72 @@ class _CanvasAreaState<CanvasArea> extends State {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: _getStack()
-    );
+    return Stack(children: _getStack());
   }
 
   List<Widget> _getStack() {
     List<Widget> widgetsOnStack = List();
+    _stopwatch.start();
 
     widgetsOnStack.add(_getBackground());
     widgetsOnStack.addAll(_getBeer());
-    widgetsOnStack.add(
-      Positioned(
+    widgetsOnStack.add(Positioned(
         right: 16,
         top: 16,
         child: Text(
           'Score: $score',
-          style: TextStyle(
-            fontSize: 24
-          ),
-        )
-      )
-    );
-
+          style: TextStyle(fontSize: 20),
+        )));
+    widgetsOnStack.add(Positioned(
+        left: 16,
+        top: 16,
+        child: Text(formatTime(_stopwatch.elapsedMilliseconds),
+            style: TextStyle(fontSize: 20))));
     return widgetsOnStack;
   }
 
   Container _getBackground() {
     return Container(
       decoration: new BoxDecoration(
-        gradient: new RadialGradient(
-          stops: [0.2, 1.0],
-          colors: [
-            Color(0xff5ef2ff),
-            Color(0xff4975de)
-          ],
-        )
-      ),
+          gradient: new RadialGradient(
+        stops: [0.2, 1.0],
+        colors: [Color(0xff5ef2ff), Color(0xff4975de)],
+      )),
     );
   }
-
-
 
   List<Widget> _getBeer() {
     List<Widget> list = new List();
 
     for (Beer beer in beers) {
-      list.add(
-        Positioned(
-          top: beer.position.dy,
-          left: beer.position.dx,
-          child: Transform.rotate(
-            angle: beer.rotation * pi * 2,
-              child: GestureDetector(
-                onTap: ()  {
-                  beers.remove(beer);
-                  score += 10;
-                },
-                child: Container(
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20.0),
-                      child: _getBeerGlass(beer)
-                  ),
-                ),
-              ),
+      list.add(Positioned(
+        top: beer.position.dy,
+        left: beer.position.dx,
+        child: Transform.rotate(
+          angle: beer.rotation * pi * 2,
+          child: GestureDetector(
+            onTap: () {
+              beers.remove(beer);
+              score += 10;
+            },
+            child: Container(
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20.0),
+                  child: _getBeerGlass(beer)),
+            ),
           ),
-        )
-      );
+        ),
+      ));
     }
 
     return list;
   }
 
   Widget _getBeerGlass(Beer beer) {
-    return Image.asset(
-      'assets/beer.png',
-      height: 80,
-      fit: BoxFit.fitHeight
-    );
+    return Image.asset('assets/beer.png', height: 80, fit: BoxFit.fitHeight);
   }
 
-
-
-
-
- /* _checkCollision() {
+/* _checkCollision() {
     if (touchSlice == null) {
       return;
     }
@@ -161,8 +151,5 @@ class _CanvasAreaState<CanvasArea> extends State {
       }
     }
   }*/
-
-
-
 
 }
